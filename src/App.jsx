842a8ace1d54453202fd331a6fbd00b0107ac89b
Olusola Afikode <afikodeolaitan@gmail.com>
2023-10-18 16:55:51 +0100

    create pages and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
 function App() {
   return (
-    <>
-      <h1 className="text-red-500 text-center">Hello, Ilumi</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route to="/" element={<Home />} />
+        <Route to="/about" element={<About />} />
+        <Route to="/profile" element={<Profile />} />
+        <Route to="/sign-in" element={<SignIn />} />
+        <Route to="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
