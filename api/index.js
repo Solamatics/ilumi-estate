e48043d334749336fbe9a2cfda90cc723f9f13b7
Olusola Afikode <afikodeolaitan@gmail.com>
2023-10-24 11:46:33 +0100

    Connet to Database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MONGODB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log(`Server listening on port: 3000`)
-})
+  console.log(`Server listening on port: 3000`);
+});
