7c1f2c1872a5cb94398921fd56ea80fcc72f5d6b
Olusola Afikode <afikodeolaitan@gmail.com>
2023-10-24 13:37:53 +0100

    create signup route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from "./routes/user.js";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 const app = express();
 
@@ -17,6 +18,7 @@ mongoose
   });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(3000, () => {
   console.log(`Server listening on port: 3000`);
