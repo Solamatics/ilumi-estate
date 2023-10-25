0c4939ecb1f453a81705b42d810b7327bcd05105
Olusola Afikode <afikodeolaitan@gmail.com>
2023-10-25 07:00:44 +0100

    add middleware and function to handle possible error

--- api/index.js
+++ api/index.js
@@ -20,6 +20,13 @@ mongoose
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
+//middleware for error handling
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({ success: false, statusCode, message });
+});
+
 app.listen(3000, () => {
   console.log(`Server listening on port: 3000`);
 });
