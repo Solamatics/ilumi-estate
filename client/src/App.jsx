7a5e37a38270a4e6c99c3751ce4fb45475d7f546
Olusola Afikode <afikodeolaitan@gmail.com>
2023-10-26 14:14:26 +0100

    complete signup page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
+import Errorpage from "./pages/errorpage";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/profile" element={<Profile />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route path="*" element={<Errorpage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/pages/Errorpage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Errorpage = () => {
+  return (
+    <div className="text-center">
+      <p>
+        <span>404!</span>Route does not exist
+      </p>
+    </div>
+  );
+};
+
+export default Errorpage;
