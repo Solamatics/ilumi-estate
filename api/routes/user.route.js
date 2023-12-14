2b861a59ffcbc9e3e1ab4f85a39d5d6528d76566
Olusola Afikode <afikodeolaitan@gmail.com>
2023-12-14 18:03:19 +0100

    create get listing api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,6 +3,7 @@ import {
   updateUserInfo,
   getUser,
   deleteUser,
+  getUserListings,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.post("/update/:id", verifyToken, updateUserInfo);
 router.get("/getuser/:id", getUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listings/:id", verifyToken, getUserListings);
 
 export default router;
