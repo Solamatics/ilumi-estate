b8417d802d4c5c48c74c832953d99bb6e1fdcd89
Olusola Afikode <afikodeolaitan@gmail.com>
2023-12-16 09:27:58 +0100

    update listing api route created

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
   try {
@@ -22,7 +23,30 @@ export const deleteListing = async (req, res, next) => {
 
   try {
     await Listing.findByIdAndDelete(req.params.id);
-    res.status(200).json("Listing has been delete")
+    res.status(200).json("Listing has been delete");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found"));
+  }
+
+  if (req.user.userId !== listing.userRef) {
+    return next(errorHandler(401, "You can only update your own listing"));
+  }
+
+  try {
+    const updateListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true },
+    );
+    res.status(200).json(updateListing);
   } catch (error) {
     next(error);
   }
