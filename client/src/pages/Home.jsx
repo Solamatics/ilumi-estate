9a640d19cca8ed9c316873b07a5037340c545339
Olusola Afikode <afikodeolaitan@gmail.com>
2023-12-29 12:55:12 +0100

    complete homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,165 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import ListingCard from "../components/ListingCard";
+import axios from "axios";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
+import { Navigation } from "swiper/modules";
+import "swiper/css/bundle";
+
 const Home = () => {
-  return <div>
-    <p>Welcome to ilumi Homes</p>
-  </div>;
+  const [saleListings, setSaleListings] = useState([]);
+  const [offerListings, setOfferListings] = useState([]);
+  const [rentListings, setRentListings] = useState([]);
+
+  const URL = import.meta.env.VITE_BASE_URL;
+
+  SwiperCore.use([Navigation]);
+  //fetch sale listings
+  const fetchOfferListings = async () => {
+    try {
+      const response = await axios.get(
+        `${URL}/api/listing/get?offer=true&limit=4`,
+      );
+      const data = response.data;
+      setOfferListings(data);
+      fetchRentListings();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchRentListings = async () => {
+    try {
+      const response = await axios.get(
+        `${URL}/api/listing/get?type=rent&limit=4`,
+      );
+      const data = response.data;
+      setRentListings(data);
+      fetchSaleListings();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchSaleListings = async () => {
+    try {
+      const response = await axios.get(
+        `${URL}/api/listing/get?type=sale&limit=4`,
+      );
+      const data = response.data;
+      setSaleListings(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchOfferListings();
+  }, []);
+
+  return (
+    <div>
+      <div className="flex flex-col gap-6 py-28 px-3 max-w-6xl mx-auto">
+        <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
+          Find your next <span className="text-slate-500">perfect</span>
+          <br /> place with ease
+        </h1>
+        <div className="text-gray-400 text-xs sm:text-sm">
+          Ilumi Esatte: The heart of this home is its well-appointed kitchen,
+          featuring top-of-the-line
+          <br /> appliances, ample counter space, and stylish cabinetry.
+        </div>
+        <Link
+          to="/search"
+          className="text-xs sm:text-sm text-blue-800 font-bold hover:underline"
+        >
+          Let's get started
+        </Link>
+      </div>
+      <Swiper navigation>
+        {offerListings?.length > 0 &&
+          offerListings.map((listing) => (
+            <SwiperSlide key={listing._id}>
+              <div
+                style={{
+                  background: `url(${listing.imageUrls[0]}) center no-repeat`,
+                  backgroundSize: "cover",
+                }}
+                className="h-[500px]"
+                key={listing._id}
+              ></div>
+            </SwiperSlide>
+          ))}
+      </Swiper>
+
+      {/*listings */}
+      <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10">
+        {offerListings && offerListings.length > 0 && (
+          <div className="">
+            <div className="my-3">
+              <h2 className="text-2xl font-semibold text-slate-600">
+                Recent Offers
+              </h2>
+              <Link
+                className="text-sm text-blue-800 hover:underline"
+                to={`/search?offer=true`}
+              >
+                Show more offers
+              </Link>
+            </div>
+            <div className="flex flex-wrap items-center gap-4">
+              {offerListings.map((listing) => (
+                <ListingCard listing={listing} key={listing._id} />
+              ))}
+            </div>
+          </div>
+        )}
+
+        {rentListings && rentListings.length > 0 && (
+          <div className="">
+            <div className="my-3">
+              <h2 className="text-2xl font-semibold text-slate-600">
+                Recent Places for rent
+              </h2>
+              <Link
+                className="text-sm text-blue-800 hover:underline"
+                to={`/search?type=rent`}
+              >
+                Show more Places for rent
+              </Link>
+            </div>
+            <div className="flex flex-wrap items-center gap-4">
+              {rentListings.map((listing) => (
+                <ListingCard listing={listing} key={listing._id} />
+              ))}
+            </div>
+          </div>
+        )}
+
+        {saleListings && saleListings.length > 0 && (
+          <div className="">
+            <div className="my-3">
+              <h2 className="text-2xl font-semibold text-slate-600">
+                Recent Places for sale
+              </h2>
+              <Link
+                className="text-sm text-blue-800 hover:underline"
+                to={`/search?type=sale`}
+              >
+                Show more Places for sale
+              </Link>
+            </div>
+            <div className="flex flex-wrap items-center gap-4">
+              {saleListings.map((listing) => (
+                <ListingCard listing={listing} key={listing._id} />
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
