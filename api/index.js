07995f28fb7a345b2889d8e6645313bd7de4a617
Olusola Afikode <afikodeolaitan@gmail.com>
2023-12-29 13:45:21 +0100

    add pre installation commad

--- api/index.js
+++ api/index.js
@@ -6,12 +6,15 @@ import userRouter from "./routes/user.route.js";
 import listingRouter from "./routes/listing.route.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors());
+
+const __dirname = path.resolve();
 
 mongoose
   .connect(process.env.MONGO_URI)
@@ -25,6 +28,12 @@ mongoose
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 //middleware for error handling
 app.use((err, req, res, next) => {
